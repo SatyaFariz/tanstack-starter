d265389d8b57bed5687482d73572c96692ee394a
Satya Fariz <satyafariznur@gmail.com>
2025-05-25 00:06:41 +0700

    chore: install and configure tailwind css

--- web/app/routes/index.tsx
+++ web/app/routes/index.tsx
@@ -1,6 +1,4 @@
 // app/routes/index.tsx
-import Http from '@/utils/http';
-import { useQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
@@ -8,21 +6,16 @@ export const Route = createFileRoute('/')({
 });
 
 function Home() {
-  const { data } = useQuery({
-    queryKey: ['products'],
-    queryFn: () =>
-      Http.get<object>('https://dummyjson.com/products'),
-  });
   return (
     <div>
-      <h1>Products</h1>
-      {data ? (
-        <div>
-          {JSON.stringify(data)}
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <button
+        onClick={() => {
+          alert('Hello, world!');
+        }}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Click Me
+      </button>
     </div>
   );
 }
