f3b9b7654f1a2a690a8ae0db46dfc6e8d9e6cc0e
Satya Fariz <satyafariznur@gmail.com>
2025-06-13 06:22:39 +0700

    update comment text

--- web/db/init-scripts/init-staging-db.sh
+++ web/db/init-scripts/init-staging-db.sh
@@ -8,7 +8,7 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE ${POSTGRES_DB}_staging;
     
-    -- Grant all privileges to the same user for the new databases
+    -- Grant all privileges to the same user for the staging database
     GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB}_staging TO $POSTGRES_USER;
 EOSQL
 
