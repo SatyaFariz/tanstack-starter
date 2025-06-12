fa8c13d5a1edccacf34841a8a6be7d073962c6d2
Satya Fariz <satyafariznur@gmail.com>
2025-06-13 06:21:02 +0700

    add db directory, move docker-compose.yml and docker-compose.dev.yml to db directory and add init script to create staging db

---
+++ web/db/init-scripts/init-staging-db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Create staging database using environment variables
+# The staging database uses the same user as the production database
+# It also uses the production database name + suffix _staging
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE ${POSTGRES_DB}_staging;
+    
+    -- Grant all privileges to the same user for the new databases
+    GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB}_staging TO $POSTGRES_USER;
+EOSQL
+
+echo "Staging database created successfully!"
