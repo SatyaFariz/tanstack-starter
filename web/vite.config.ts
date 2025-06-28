833be4d0f76d810e800d22fba8582f98572a41eb
Satya Fariz <satyafariznur@gmail.com>
2025-06-29 00:45:55 +0700

    chore: upgrade tanstack start

---
+++ web/vite.config.ts
@@ -0,0 +1,16 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+  server: {
+    port: 3001,
+  },
+  plugins: [
+    svgr(),
+    tsConfigPaths(), 
+    tanstackStart()
+  ],
+})
