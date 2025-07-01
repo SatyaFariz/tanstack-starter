0718abbd4c267a2b2ece8cf6d897b1d550318aa3
Satya Fariz <satyafariznur@gmail.com>
2025-07-01 08:30:26 +0700

    chore: add spinner default variant

--- web/src/components/ui/spinner.tsx
+++ web/src/components/ui/spinner.tsx
@@ -1,6 +1,7 @@
 import cn from "@/utils/cn"
 
 type SpinnerProps = {
+  variant?: 'default' | 'ring' | 'ios' | 'dots'
   className?: string;
   classNames?: {
     root?: string;
@@ -9,28 +10,38 @@ type SpinnerProps = {
 }
 
 const Spinner = ({
+  variant,
   className,
   classNames,
 }: SpinnerProps) => {
+  if (variant === 'ios') {
+    return (
+      <div className={cn('relative flex w-8 h-8', classNames?.root)}>
+        {[...new Array(12)].map((_, index) => (
+          <i
+            key={`star-${index}`}
+            className={cn(
+              'absolute animate-fade-out rounded-full w-[25%] h-[8%] left-[calc(37.5%)] top-[calc(46%)] spinner-bar-animation bg-primary',
+              classNames?.spinner || className,
+            )}
+            style={
+              {
+                "--bar-index": index,
+                animation: 'fade-out 1.2s linear 0s infinite normal none running',
+                animationDelay: 'calc(-1.2s + (.1s * var(--bar-index)))',
+                transform: 'rotate(calc(30deg * var(--bar-index)))translate(140%)'
+              } as React.CSSProperties
+            }
+          />
+        ))}
+      </div>
+    )
+  }
+
   return (
-    <div className={cn('relative flex w-8 h-8', classNames?.root)}>
-      {[...new Array(12)].map((_, index) => (
-        <i
-          key={`star-${index}`}
-          className={cn(
-            'absolute animate-fade-out rounded-full w-[25%] h-[8%] left-[calc(37.5%)] top-[calc(46%)] spinner-bar-animation bg-primary',
-            className || classNames?.spinner,
-          )}
-          style={
-            {
-              "--bar-index": index,
-              animation: 'fade-out 1.2s linear 0s infinite normal none running',
-              animationDelay: 'calc(-1.2s + (.1s * var(--bar-index)))',
-              transform: 'rotate(calc(30deg * var(--bar-index)))translate(140%)'
-            } as React.CSSProperties
-          }
-        />
-      ))}
+    <div className="relative flex w-8 h-8">
+      <i className="absolute w-full h-full rounded-full border-3 border-b-primary animate-spinner-ease-spin border-solid border-t-transparent border-l-transparent border-r-transparent"></i>
+      <i className="absolute w-full h-full rounded-full border-3 border-b-primary opacity-75 animate-spinner-linear-spin border-dotted border-t-transparent border-l-transparent border-r-transparent"></i>
     </div>
   )
 }
