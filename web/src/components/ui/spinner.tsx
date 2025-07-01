507c573a8c853e43a19993feb6f67e1340a3f0cb
Satya Fariz <satyafariznur@gmail.com>
2025-07-01 09:35:03 +0700

    chore: add spinner ring variant

--- web/src/components/ui/spinner.tsx
+++ web/src/components/ui/spinner.tsx
@@ -30,6 +30,17 @@ const Spinner = ({
         ))}
       </div>
     )
+  } else if (variant === 'ring') {
+    return (
+      <svg 
+        className={cn('relative flex animate-spin w-8 h-8 text-primary', className)}
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle className="absolute w-full h-full rounded-full border-3 border-b-primary opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path className="absolute w-full h-full rounded-full border-3 border-b-primary opacity-75" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" fill="currentColor"></path>
+      </svg>
+    )
   }
 
   return (
