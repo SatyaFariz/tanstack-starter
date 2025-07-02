b657d8fc5c6b3256884cc72a0c15a7e8cb6c9c7b
Satya Fariz <satyafariznur@gmail.com>
2025-07-03 00:01:19 +0700

    chore: implement size prop for ring variant

--- web/src/components/ui/spinner.tsx
+++ web/src/components/ui/spinner.tsx
@@ -15,7 +15,7 @@ const Spinner = ({
     return (
       <div
         className={cn(
-          'bg-primary',
+          'text-primary',
           className,
           'relative',
           size === 'sm' && 'w-5 h-5',
@@ -44,12 +44,30 @@ const Spinner = ({
   } else if(variant === 'ring') {
     return (
       <svg
-        className={cn('relative flex animate-spin w-8 h-8 text-primary', className)}
+        className={cn(
+          'text-primary',
+          className,
+          'relative animate-spin',
+          size === 'sm' && 'w-5 h-5',
+          size === 'md' && 'w-8 h-8',
+          size === 'lg' && 'w-10 h-10',
+        )}
         fill="none"
         viewBox="0 0 24 24"
       >
-        <circle className="absolute w-full h-full rounded-full border-3 border-b-primary opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-        <path className="absolute w-full h-full rounded-full border-3 border-b-primary opacity-75" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" fill="currentColor"></path>
+        <circle
+          className="absolute w-full h-full rounded-full opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        />
+        <path
+          className="absolute w-full h-full rounded-full opacity-75"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          fill="currentColor"
+        />
       </svg>
     );
   } else if(variant === 'dots') {
